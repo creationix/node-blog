7bd8adb7b530ef909e5b0a142cf99033a56a33d3
Tim Caswell <tim@creationix.com>
2010-01-30 02:45:54 -0600

    Fix builder hook

--- builder.js
+++ builder.js
@@ -11,19 +11,29 @@ Http.createServer(function (req, res) {
   });
   req.addListener('complete', function () {
     try {
+      puts(body);
       body = Url.parse("?" + body, true).query.payload;
-      p(body);    
+      puts(body);
       body = JSON.parse(body);
-      p(body);
-      res.sendHeader(200, {'Content-Type': 'text/plain'});
-      res.sendBody('Thanks for the tip!');
-      res.finish();
     } catch(e) {
       res.sendHeader(500, {'Content-Type': 'text/plain'});
-      res.sendBody('Problem reading post message!');
+      res.sendBody('Problem reading post message!\n' + e.stack);
+
       res.finish();
+      return;
     }
+    res.sendHeader(200, {'Content-Type': 'text/plain'});
+    res.sendBody('Thanks for the tip!');
+    build(body);
+    res.finish();
   });
 
 }).listen(4242);
+
+function build(data) {
+  p(data);
+  exec("git --git-dir=data/.git pull origin master").addCallback(function (stdout, stderr) {
+    puts(stdout);
+  });
+}
 puts('Server running at http://127.0.0.1:4242/');
