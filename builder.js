15209d22af0524545891bc1265ca5d090f868d37
Tim Caswell <tim@creationix.(none)>
2010-01-30 08:27:20 +0000

    Parse the json message from github

--- builder.js
+++ builder.js
@@ -1,22 +1,29 @@
 var Markdown = require('markdown'),
-    sys = require('sys'),
-    http = require('http');
+    Url = require('url'),
+    Http = require('http');
+process.mixin(require('sys'));
 
-http.createServer(function (req, res) {
-  res.sendHeader(200, {'Content-Type': 'text/plain'});
-  res.sendBody('Thanks for the tip!');
-  res.finish();
-  sys.p(req);
-  sys.puts("Got an update tip, do something please.");
+Http.createServer(function (req, res) {
   var body = "";
   req.setBodyEncoding('utf8');
   req.addListener('body', function (chunk) {
     body += chunk;
   });
   req.addListener('complete', function () {
-    body = JSON.parse(body);
+    try {
+      body = Url.parse("?" + body, true).query.payload;
+      p(body);    
+      body = JSON.parse(body);
+      p(body);
+      res.sendHeader(200, {'Content-Type': 'text/plain'});
+      res.sendBody('Thanks for the tip!');
+      res.finish();
+    } catch(e) {
+      res.sendHeader(500, {'Content-Type': 'text/plain'});
+      res.sendBody('Problem reading post message!');
+      res.finish();
+    }
   });
-  sys.p(body);
 
 }).listen(4242);
-sys.puts('Server running at http://127.0.0.1:4242/');
+puts('Server running at http://127.0.0.1:4242/');
